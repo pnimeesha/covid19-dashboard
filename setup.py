029fa2e81663cef9ec9022c932716a5db8e638dc
pnimeesha <37923556+pnimeesha@users.noreply.github.com>
2022-07-29 16:43:18 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of COVID-19 cases and Vaccination',
+    author='Devi Athunuru',
+    license='',
+)
